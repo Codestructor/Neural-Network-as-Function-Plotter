a253cd893524cf2ac28beb4903b1e2dc6a9c3994
aoancea <alx.oancea@gmail.com>
2016-07-23 22:41:12 +0300

    Enhance Neuron class

--- Neural Network - Function Plotter/Neuron.cs
+++ Neural Network - Function Plotter/Neuron.cs
@@ -7,21 +7,26 @@ namespace Neural_Network___Function_Plotter
 {
     public class Neuron
     {
+        private List<Connection> m_outputWeights = new List<Connection>();
+
+        private readonly int myIndex;
+
         public Neuron(int numOutputs, int myIndex, Random rand)
         {
+            this.myIndex = myIndex;
+
             //Does not include the bias neuron as it receives no inputs
             for (int c = 0; c < numOutputs; c++) //c -> connection
             {
                 Connection connection = new Connection();
+
                 if (rand.Next(0, 9999) % 2 == 0)
                     connection.Weight = rand.NextDouble();
                 else
                     connection.Weight = -rand.NextDouble();
-                m_outputWeights.Add(connection);
-                //Console.WriteLine(con.weight.ToString());
-            }
 
-            m_myIndex = myIndex;
+                m_outputWeights.Add(connection);
+            }
         }
 
         private double input;
@@ -62,14 +67,14 @@ namespace Neural_Network___Function_Plotter
             for (int n = 0; n < prevLayer.neurons.Count(); n++)
             {
                 Neuron neuron = prevLayer.neurons[n];
-                double oldDeltaWeight = neuron.getConnection(m_myIndex).DeltaWeight;
+                double oldDeltaWeight = neuron.getConnection(myIndex).DeltaWeight;
 
                 //eta = overall learning rate
                 //alpha = momentum -> adds a fraction of the previous deltaweight
                 double newDeltaWeight = eta * neuron.Input * m_gradient + alpha * oldDeltaWeight;
 
-                neuron.m_outputWeights[m_myIndex].DeltaWeight = newDeltaWeight;
-                neuron.m_outputWeights[m_myIndex].Weight += newDeltaWeight;
+                neuron.m_outputWeights[myIndex].DeltaWeight = newDeltaWeight;
+                neuron.m_outputWeights[myIndex].Weight += newDeltaWeight;
             }
         }
 
@@ -79,7 +84,7 @@ namespace Neural_Network___Function_Plotter
 
             for (int n = 0; n < prevLayer.neurons.Count(); n++)
             {
-                input += prevLayer.neurons[n].Output * prevLayer.neurons[n].getConnection(m_myIndex).Weight;
+                input += prevLayer.neurons[n].Output * prevLayer.neurons[n].getConnection(myIndex).Weight;
             }
 
             output = MathUtils.TransferFunction(input);
@@ -88,8 +93,6 @@ namespace Neural_Network___Function_Plotter
         //Private
 
         private double m_gradient;
-        private int m_myIndex;
-        private List<Connection> m_outputWeights = new List<Connection>();
 
         private double sumDOW(Layer nexLayer)
         {
